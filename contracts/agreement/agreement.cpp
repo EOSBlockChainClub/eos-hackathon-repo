0c62955456dd319708b21acfa590941e0345e4a3
Anil <anil@b9lab.com>
2018-09-22 18:28:17 +0100

    Further writing on cpp.

--- contracts/agreement/agreement.cpp
+++ contracts/agreement/agreement.cpp
@@ -1,17 +1,38 @@
 #include <eosiolib/eosio.hpp>
+#include <string>
 #include <eosiolib/print.hpp>
 
 using namespace eosio;
+using namespace std;
 
 class agreement : public contract {
+    private:
+
+        struct agreedstruct {
+            account_name requester;
+            account_name dataprovider;
+            std::string agreement;
+            std::string data;
+        };
+
     public:
 
         using contract::contract;
 
         // @ abi action
-        void test() {
-            print("Test");
-        }        
+        void sendagreement(string hash);
+
+        // @ abi action
+        void senddata(string hash);
+
 };
 
-EOSIO_ABI(agreement, (test))
+void agreement::sendagreement(string hash) {
+    
+}
+
+void agreement::senddata(string hash) {
+
+}
+
+EOSIO_ABI(agreement, (senddatahash)(sendagreementhash))
