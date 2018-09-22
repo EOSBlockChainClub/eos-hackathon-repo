e452db9fc2162a931e4ddfdad5c14ef51ece9d96
Anil <anil@b9lab.com>
2018-09-22 17:54:44 +0100

    Added key creation line.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,3 +2,4 @@ eosiocpp -o /opt/eosio/bin/contracts/agreement/agreement.wast  /opt/eosio/bin/co
 eosiocpp -g /opt/eosio/bin/contracts/agreement/agreement.abi  /opt/eosio/bin/contracts/agreement/agreement.cpp
 
 cleos wallet create --file /opt/eosio/bin/keys/masterpass.txt
+cleos create key --file /opt/eosio/bin/keys/userkey.txt
