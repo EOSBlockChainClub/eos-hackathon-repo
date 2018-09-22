11570f6f017404d9cf329d7b20a9aec329914cc9
Anil <anil@b9lab.com>
2018-09-22 19:35:09 +0100

    beta

--- contracts/agreement/agreement.cpp
+++ contracts/agreement/agreement.cpp
@@ -8,31 +8,59 @@ using namespace std;
 class agreement : public contract {
     private:
 
+        ///@abi table transfers
         struct agreedstruct {
+            uint64_t prim_key;
+
             account_name requester;
             account_name dataprovider;
             std::string agreement;
             std::string data;
+
+            uint64_t primary_key() const { return prim_key; }
         };
+
+        eosio::multi_index< N(transfers), agreedstruct> transfers;
 
     public:
 
-        using contract::contract;
+        agreement(account_name self): contract(self), transfers(self, self) {}
 
         // @ abi action
-        void sendagreement(string hash);
+        void sendagr(account_name requester, account_name dataprovider, string hash);
 
         // @ abi action
-        void senddata(string hash);
+        void senddata(account_name dataprovider, uint64_t prim_key, string datahash);
+
 
 };
 
-void agreement::sendagreement(string hash) {
-    
+void agreement::sendagr(account_name requester, account_name dataprovider, string hash) {
+    require_auth(requester);
+
+    transfers.emplace(requester, [&](auto& agreed) {
+
+        agreed.prim_key = transfers.available_primary_key();
+
+        agreed.requester = requester;
+        agreed.dataprovider = dataprovider;
+        agreed.agreement = hash;
+    });
 }
 
-void agreement::senddata(string hash) {
+void agreement::senddata(account_name dataprovider, uint64_t prim_key, string datahash) {
+    require_auth(dataprovider);
 
+    auto itr = transfers.find(prim_key);
+
+    eosio_assert(itr != transfers.end(), "Agreement not found");
+    eosio_assert(itr->dataprovider == dataprovider, "Data provider not allowed");
+    eosio_assert(itr->data.size() == 0, "Data already provided");
+
+    transfers.modify(itr, dataprovider, [&](auto& agreed) {
+        agreed.data = datahash;
+    });
 }
 
-EOSIO_ABI(agreement, (senddatahash)(sendagreementhash))
+
+EOSIO_ABI(agreement, (sendagr)(senddata))
