58b256bdb889e74d78cbebacd704107a54f12ef9
Sam Schmeink <sam@b9lab.com>
2018-09-22 19:50:24 +0100

    insert agreement script, wip

---
+++ scripts/insertAgreement.sh
@@ -0,0 +1,17 @@
+echo "Please enter the username of the requester"
+read requester
+
+echo "Please enter the username of the dataprovider"
+
+read dataprovider
+
+echo "Create new agreement, please enter file path"
+read path
+
+hashString="$(shasum -a 256 $path)"
+
+hash="$(echo $hashString | cut -d' ' -f1)" # get hash without filename
+
+echo $hash
+
+cleos push action agreement sendagr '["$requester", "$dataprovider", "$hash"]' -p $requester@active
