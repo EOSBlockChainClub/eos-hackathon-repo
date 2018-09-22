d0a08c7110474020bd4d31c38f744d69c8999d97
Anil <anil@b9lab.com>
2018-09-22 18:10:31 +0100

    init account creation

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,14 @@ eosiocpp -o /opt/eosio/bin/contracts/agreement/agreement.wast  /opt/eosio/bin/co
 eosiocpp -g /opt/eosio/bin/contracts/agreement/agreement.abi  /opt/eosio/bin/contracts/agreement/agreement.cpp
 
 cleos wallet create --file /opt/eosio/bin/keys/masterpass.txt
-cleos create key --file /opt/eosio/bin/keys/userkey.txt
+cleos create key --file /opt/eosio/bin/keys/userkeyactive.txt
+cleos create key --file /opt/eosio/bin/keys/userkeyowner.txt
+
+cleos wallet import --private-key 5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3 # default private key of eosio
+
+pbkeya="$(grep Public ./keys/userkeyactive.txt | awk '{print $3}')"
+pvkeya="$(grep Private ./keys/userkeyactive.txt | awk '{print $3}')"
+pbkeyo="$(grep Public ./keys/userkeyowner.txt | awk '{print $3}')"
+pvkeyo="$(grep Private ./keys/userkeyowner.txt | awk '{print $3}')"
+
+cleos create account eosio user ${pbkeya} ${pbkeyo}
