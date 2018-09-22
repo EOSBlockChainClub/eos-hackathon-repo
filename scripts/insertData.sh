f5e4c748826d782252c4cff36b6d50271e630991
Sam Schmeink <sam@b9lab.com>
2018-09-22 21:18:21 +0100

    insertData.sh added, output for insertAgreement

--- scripts/insertData.sh
+++ scripts/insertData.sh
@@ -1,17 +1,18 @@
-$echo "Please enter the username of the requester"
-read requester
-
 echo "Please enter the username of the dataprovider"
-
 read dataprovider
 
-echo "Create new agreement, please enter file path"
+echo "Please enter the primary key of transaction"
+
+read key
+
+echo "Create new agreement, please enter data file path"
 read path
 
 hashString="$(shasum -a 256 $path)"
+
 hash="$(echo $hashString | cut -d' ' -f1)" # get hash without filename
 
-cleos push action agreement sendagr '["'${requester}'", "'${dataprovider}'", "'${hash}'"]' -p $requester@active
+cleos push action agreement senddata '["'${dataprovider}'", '$key', "'${hash}'"]' -p $dataprovider@active
 
 echo "Get the rows"
 cleos get table agreement agreement transfers
