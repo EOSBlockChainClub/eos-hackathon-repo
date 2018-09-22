2aba7db6388514c6dba096f0ec7c09dba2dd7216
Xavier Lepretre <xavier.lepretre@gmail.com>
2018-09-22 22:43:42 +0100

    Script attempt at requesting agreement.

---
+++ app/insertAgreement.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+const Eos = require("eosjs");
+const allKeys = require("./pickKeys.js")();
+
+const requesterAcc = "requester";
+const dataProviderAcc = "dataprovider";
+
+const hashString = "Hahash";
+
+const eos = Eos({
+    keyProvider: allKeys.requesterActive.privateKey
+});
+
+eos.contract("agreement")
+    .then(agreement => agreement.sendagr(
+        { 
+            requester: requesterAcc,
+            dataprovider: dataProviderAcc,
+            hash: hashString
+        },
+        {
+            scope: "agreement",
+            authorization: [{
+                actor: requesterAcc,
+                permission: 'active',
+            }]
+        }))
+    .then(console.log)
+    .catch(console.error);
