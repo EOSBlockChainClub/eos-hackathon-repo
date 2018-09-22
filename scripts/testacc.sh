52e6de7ae309071c719be6c1b9993099bacfca8b
Anil <anil@b9lab.com>
2018-09-22 22:50:50 +0100

    create test accounts

---
+++ scripts/testacc.sh
@@ -0,0 +1,17 @@
+# Create test accounts
+
+mkdir -p ./keys/testkeys
+
+for i in {a..j}
+do
+  cleos create key --file /opt/eosio/bin/keys/testkeys/testkeyactive$i.txt
+  cleos create key --file /opt/eosio/bin/keys/testkeys/testkeyowner$i.txt
+
+  pbkey="$(grep Public ./keys/testkeys/testkeyactive$i.txt | awk '{print $3}')"
+  pvkey="$(grep Private ./keys/testkeys/testkeyowner$i.txt | awk '{print $3}')"
+
+  cleos wallet import --private-key ${pvkey}
+
+  cleos create account eosio test$i ${pbkey} ${pbkey}
+
+done
