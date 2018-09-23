3a76e852504888104cbacd072f672affa712aed0
Anil <anil@b9lab.com>
2018-09-23 10:30:55 +0000

    Update testdata.sh

--- scripts/testdata.sh
+++ scripts/testdata.sh
@@ -1,30 +1,99 @@
+cleos wallet unlock --password PW5K2g2HyZxm44Vu74aSL9x3GoB354YDMTUXENhcEaP9ykYVU9DZK
 
-agreementPath=/agreement.txt
-dataPath=/data.txt
-
-agreementHashString="$(shasum -a 256 $agreementPath)"
-agreementHash="$(echo $agreementHashString | cut -d' ' -f1)"
-
-dataHashString="$(shasum -a 256 $dataPath)"
-dataHash="$(echo $dataHashString | cut -d' ' -f1)"
-
-for i in a..j
+### User Pool ####
+users=()
+for i in {a..i}
 do
-    requester=testacc$i
-    dataprovider=DATA$i
-    key=1
-
-    # Insert agreement
-    cleos push action agreement sendagr '["'${requester}'", "'${dataprovider}'", "'${agreementHash}'"]' -p $requester@active
-
-    # Insert data
-    cleos push action agreement senddata '["'${dataprovider}'", '$key', "'${dataHash}'"]' -p $dataprovider@active
+    users+=(test$i)
 done
 
 
+data="data1"
+agreement="agreement"
+
+### Path 1 START ### 
+# A -> B 
+requester=${users[1]}
+dataprovider=${users[0]}
+key=0
+
+# Insert agreement
+cleos push action agreement sendagr '["'${requester}'", "'${dataprovider}'", "'${agreement}'"]' -p $requester@active
+# Insert data
+cleos push action agreement senddata '["'${dataprovider}'", '$key', "'${data}'"]' -p $dataprovider@active
+
+# Path B -> C
+requester=${users[2]}
+dataprovider=${users[1]}
+key=1
+
+# Insert agreement
+cleos push action agreement sendagr '["'${requester}'", "'${dataprovider}'", "'${agreement}'"]' -p $requester@active
+# Insert data
+cleos push action agreement senddata '["'${dataprovider}'", '$key', "'${data}'"]' -p $dataprovider@active
+
+# Path C -> E
+requester=${users[4]}
+dataprovider=${users[2]}
+key=2
+
+# Insert agreement
+cleos push action agreement sendagr '["'${requester}'", "'${dataprovider}'", "'${agreement}'"]' -p $requester@active
+# Insert data
+cleos push action agreement senddata '["'${dataprovider}'", '$key', "'${data}'"]' -p $dataprovider@active
+
+# Path E -> F
+requester=${users[5]}
+dataprovider=${users[4]}
+key=3
+
+# Insert agreement
+cleos push action agreement sendagr '["'${requester}'", "'${dataprovider}'", "'${agreement}'"]' -p $requester@active
+# Insert data
+cleos push action agreement senddata '["'${dataprovider}'", '$key', "'${data}'"]' -p $dataprovider@active
+
+# Path F -> G
+requester=${users[6]}
+dataprovider=${users[5]}
+key=4
+
+# Insert agreement
+cleos push action agreement sendagr '["'${requester}'", "'${dataprovider}'", "'${agreement}'"]' -p $requester@active
+# Insert data
+cleos push action agreement senddata '["'${dataprovider}'", '$key', "'${data}'"]' -p $dataprovider@active
+
+# Path C -> D
+requester=${users[3]}
+dataprovider=${users[2]}
+key=5
+
+# Insert agreement
+cleos push action agreement sendagr '["'${requester}'", "'${dataprovider}'", "'${agreement}'"]' -p $requester@active
+# Insert data
+cleos push action agreement senddata '["'${dataprovider}'", '$key', "'${data}'"]' -p $dataprovider@active
+
+# Path D -> H
+requester=${users[7]}
+dataprovider=${users[3]}
+key=6
+
+# Insert agreement
+cleos push action agreement sendagr '["'${requester}'", "'${dataprovider}'", "'${agreement}'"]' -p $requester@active
+# Insert data
+cleos push action agreement senddata '["'${dataprovider}'", '$key', "'${data}'"]' -p $dataprovider@active
+
+# Path H -> I
+requester=${users[8]}
+dataprovider=${users[7]}
+key=7
+
+# Insert agreement
+cleos push action agreement sendagr '["'${requester}'", "'${dataprovider}'", "'${agreement}'"]' -p $requester@active
+# Insert data
+cleos push action agreement senddata '["'${dataprovider}'", '$key', "'${data}'"]' -p $dataprovider@active
 
 
 
 # Print the table rows
 echo "Table:"
-cleos get table agreement agreement transfers
+cleos get table agreement agreement transfers --limit 100
